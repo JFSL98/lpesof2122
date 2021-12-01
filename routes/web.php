2ead5f84b10f0efec4a5820b7b912722248be96c
JFSL98 <lopes.jfsl98@gmail.com>
2021-12-01 21:15:11 +0000

    /index adicionado às routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/index', function() {
+    return view('index');
+});
+
 Route::get('/account', function() {
     return view('account');
 });
