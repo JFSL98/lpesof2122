af19bf0f5d96cfe8ae9d2d3618e986279e42832a
fpaiva36834 <36834@ufp.edu.pt>
2021-12-03 01:33:20 +0000

    Update landing page
    adicionei o imagens e alterei o favicon
    modifiquei coisas no account e tratei de fazer um footer basico

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -16,25 +16,23 @@
     <h2>O que faz esta página (account)?</h2>
     <p>Página onde poderá selecionar se quer fazer Login ou Registar, mas reencaminha para / se já estiver logged in.</p>
       -->
-    <div class="container-lg d-flex justify-content-md-center align-items-center vh-100">
+    <div class="container-lg d-flex justify-content-md-center align-items-center vh-100 text-center">
         <div class="row">
-            <div class="col">
+            <div class="col-6">
                 <!-- logotipo UFP -->
-                <p>logo</p>
+                <a href="/"><img src="{{ asset('images/UFP-Logo.png') }}" alt="logotipo UFP" class="img-fluid"></a>
             </div>
-        </div>
-        <div class="row">
             <div class="col">
-                <h3>Bem-vindo!</h3>
-                <a class="btn btn-primary" href="/login" role="button">Login</a>
-                <a class="btn btn-primary" href="/register" role="button">Registar</a>
+                        <h5 class="card-title">Bem-vindo!</h5>
+                        <a style="background-color:#006037;" class="btn btn-primary" href="/login" role="button">Login</a>
+                        <a style="background-color:#006037;" class="btn btn-primary" href="/register" role="button">Registar</a>
             </div>
         </div>
     </div>
-
+      
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    @include('includes.footer');
+    @include('includes.footer')
   </body>
 </html>
