9812731b107469cb658e63e3dbd3ae2fae635baa
JFSL98 <lopes.jfsl98@gmail.com>
2021-12-04 17:26:44 +0000

    Footer fica fixo no fundo da página

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,23 +1,18 @@
-
 @extends('layout.master')
 
-<body>
-<div class="container">
-  <footer  class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <i class="fas fa-home mr-2"></i>
-        <span class="d-none d-md-inline"></span>
-      </a>
-      <span class="text-muted">© 2021 UFP</span>
-    </div>
+<div class="container fixed-bottom">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <span class="text-muted">© 2021 UFP</span>
+        </div>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="#"><i href="www.twitter.com" class="fab fa-twitter"></i></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><i href="www.instagram.com" class="fab fa-instagram"></i></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><i href="www.facebook.com" class="fab fa-facebook"></i></a></li>
-    </ul>
-  </footer>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-muted" href="#"><i href="www.twitter.com" class="fab fa-twitter"></i></a>
+            </li>
+            <li class="ms-3"><a class="text-muted" href="#"><i href="www.instagram.com"
+                        class="fab fa-instagram"></i></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><i href="www.facebook.com" class="fab fa-facebook"></i></a>
+            </li>
+        </ul>
+    </footer>
 </div>
-</body>
-</html>
