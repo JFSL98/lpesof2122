2aa92e939c65832c5c6cf631863cb24a8454a0d0
Dropperd <36640@ufp.edu.pt>
2021-12-13 17:29:30 +0000

    admin route and adminhome page

---
+++ resources/views/adminHome.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.master')
+
+@section('content')
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h1>
+                        You are admin.
+                    </h1>
+  
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+@endsection
