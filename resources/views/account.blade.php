2d04aea1704eb9671eef67c72a1df8e3ffd9cbbb
fpaiva36834 <36834@ufp.edu.pt>
2021-12-27 18:53:57 +0000

    Botão outra ver a funcionar

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-6">
-            <!-- logotipo UFP -->
-            <a href="/"><img src="{{ asset('images/UFP-Logo.png') }}" alt="logotipo UFP" class="img-fluid"></a>
-        </div>
-        <div class="col">
-            <h5 class="card-title">Bem-vindo!</h5>
-            <a class="btn" href="{{ route('login') }}" role="button">Login</a>
-            <a class="btn" href="{{ route('register') }}" role="button">Registar</a>
-        </div>
+<div class="row">
+    <div class="col-6">
+        <!-- logotipo UFP -->
+        <a href="/"><img src="{{ asset('images/UFP-Logo.png') }}" alt="logotipo UFP" class="img-fluid"></a>
     </div>
+    <div class="col">
+        <h5 class="card-title">Bem-vindo!</h5>
+        <a class="btn btn-index" href="{{ route('login') }}" role="button">Login</a>
+        <a class="btn btn-index" href="{{ route('register') }}" role="button">Registar</a>
+    </div>
+</div>
 @endsection
