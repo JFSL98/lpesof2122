5bd4a23f6f1341657f9bb8134e1df3c7122bc181
JFSL98 <lopes.jfsl98@gmail.com>
2022-01-15 21:42:59 +0000

    Teste de adicionar amigo funcional

---
+++ tests/Feature/FriendsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Tests\TestCase;
+
+class FriendsTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function add_friend()
+    {
+        $users = User::factory(2)->create();
+        $user = $users->find(1);
+        $user2 = $users->find(2);
+
+        $this->actingAs($user);
+
+        $this->post($user2->id.'/friend/new',['friend_id' => $user2->id ]);
+        
+        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>false]);
+        $this->assertDatabaseCount('friends',1);
+
+        $this->actingAs($user2);
+
+        $this->post($user->id.'/friend/new',['friend_id' => $user->id]);
+
+        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>true]);
+        $this->assertDatabaseHas('friends',['user_id'=>$user2->id,'friend_id'=>$user->id,'validate'=>true]);
+
+        $this->assertDatabaseCount('friends',2);
+    }
+}
--- tests/Feature/PhotoTest.php
+++ tests/Feature/PhotoTest.php
@@ -23,12 +23,8 @@ class PhotoTest extends TestCase
 
         $this->actingAs($user);
 
-        
-        
-
         $file = UploadedFile::fake()->image('image.jpg');
         
-
         $response = $this->post($user->id.'/upload', [
             'image' => $file,
         ]);
@@ -36,26 +32,4 @@ class PhotoTest extends TestCase
         Storage::disk('image')->assertExists(storage_path('app\public\images\profile_pic\\').$file->hashName());
         
     }
-
-    /** @test */
-    public function add_friend(){
-
-        $user = User::factory()->create()->first();
-        $user2 =  User::factory()->create()->first();
-    
-
-        $this->actingAs($user);
-
-        $this->post($user2->id.'/friend/new',['friend_id' => $user2->id ]);
-        //$this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>false]);
-
-        $this->assertDatabaseCount('friends',1);        
-        $this->actingAs($user2);
-
-        $this->post($user->id.'/friend/new',['friend_id' => $user->id]);
-
-        $this->assertDatabaseCount('friends',2);  
-
-    }
-
 }
