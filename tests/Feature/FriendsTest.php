5130a2caa74a98d07de2d877d950fc6dbd6effbb
Dropperd <36640@ufp.edu.pt>
2022-01-16 17:35:01 +0000

    update ao teste remove friend

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
 use Tests\TestCase;
+use App\Models\Friends;
 
 class FriendsTest extends TestCase
 {
@@ -42,19 +43,20 @@ class FriendsTest extends TestCase
         $user = $users->find(1);
         $user2 = $users->find(2);
 
-        $this->actingAs($user);
+        Friends::create([
+            'user_id' => $user->id,
+            'friend_id' => $user2->id,
+            'validate' => true
+          ]);
+          
+          Friends::create([
+            'user_id' => $user2->id,
+            'friend_id' => $user->id,
+            'validate' => true
+          ]);
 
-        $this->post($user2->id.'/friend/new',['friend_id' => $user2->id ]);
         
-        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>false]);
-        $this->assertDatabaseCount('friends',1);
-
         $this->actingAs($user2);
-
-        $this->post($user->id.'/friend/new',['friend_id' => $user->id]);
-
-        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>true]);
-        $this->assertDatabaseHas('friends',['user_id'=>$user2->id,'friend_id'=>$user->id,'validate'=>true]);
 
         $this->assertDatabaseCount('friends',2);
 
