4533da8bb96732856d029d5a89ae0de8147064f6
Dropperd <36640@ufp.edu.pt>
2022-01-16 17:01:37 +0000

    teste de remover friend

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -34,4 +34,39 @@ class FriendsTest extends TestCase
 
         $this->assertDatabaseCount('friends',2);
     }
+
+    /** @test */
+    public function remove_friend()
+    {
+        $users = User::factory(2)->create();
+        $user = $users->find(1);
+        $user2 = $users->find(2);
+
+        $this->actingAs($user);
+
+        $this->post($user2->id.'/friend/new',['friend_id' => $user2->id ]);
+        
+        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>false]);
+        $this->assertDatabaseCount('friends',1);
+
+        $this->actingAs($user2);
+
+        $this->post($user->id.'/friend/new',['friend_id' => $user->id]);
+
+        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>true]);
+        $this->assertDatabaseHas('friends',['user_id'=>$user2->id,'friend_id'=>$user->id,'validate'=>true]);
+
+        $this->assertDatabaseCount('friends',2);
+
+        $this->post($user->id.'/friend/remove',['friend_id' => $user->id ]);
+        $this->assertDatabaseHas('friends',['user_id'=>$user->id,'friend_id'=>$user2->id,'validate'=>false]);
+        $this->assertDatabaseCount('friends',1);
+
+
+        $this->actingAs($user);
+        $this->post($user2->id.'/friend/remove',['friend_id' => $user2->id ]);
+        $this->assertDatabaseCount('friends',0);
+
+    }
+
 }
