c633d33c6d733c84075e64542c60f1a35dbb1b44
JFSL98 <lopes.jfsl98@gmail.com>
2022-01-16 01:45:45 +0000

    Teste de adição e troca da profile_pic funcional

--- tests/Feature/PhotoTest.php
+++ tests/Feature/PhotoTest.php
@@ -15,21 +15,48 @@ class PhotoTest extends TestCase
 
     use RefreshDatabase;
 
-    
+    /** @test */
     public function change_profile_pic()
     {
         $user = User::factory()->create()->first();
-        Storage::fake('image');
+        
+        Storage::fake('images');
+        config()->set('filesystems.disks.images', [
+            'driver' => 'local',
+            'root' => Storage::disk('images')->getAdapter()->getPathPrefix(),
+        ]);
 
         $this->actingAs($user);
 
-        $file = UploadedFile::fake()->image('image.jpg');
+        $image1 = 'image.jpg';
+
+        $file = UploadedFile::fake()->image($image1);
+
+        $firstHashName = $file->hashName();
         
-        $response = $this->post($user->id.'/upload', [
+        $this->post($user->id.'/upload', [
             'image' => $file,
         ]);
+
         $this->assertDatabaseCount('photos',1);
-        Storage::disk('image')->assertExists(storage_path('app\public\images\profile_pic\\').$file->hashName());
+        $this->assertDatabaseHas('users', ['id' => $user->id, 'profile_pic_id' => 1]);
+        $this->assertDatabaseHas('photos', ['user_id' => $user->id, 'name' => $image1, 'path' => $firstHashName]);
+        $this->assertFileExists(storage_path('app/public/images/profile_pic/'.$firstHashName));
+
+        // change profile picture
+        $image2 = 'image2.jpg';
+
+        $file = UploadedFile::fake()->image($image2);
+
+        $secondHashName = $file->hashName();
         
+        $this->post($user->id.'/upload', [
+            'image' => $file,
+        ]);
+
+        $this->assertDatabaseHas('users', ['id' => $user->id, 'profile_pic_id' => 2]);
+        $this->assertDatabaseHas('photos', ['user_id' => $user->id, 'name' => $image2, 'path' => $secondHashName]);
+        $this->assertDeleted('photos', ['user_id' => $user->id, 'name' => $image2, 'path' => $firstHashName]);
+        $this->assertFileExists(storage_path('app/public/images/profile_pic/'.$secondHashName));
     }
 }
